d327122b65b1fd59f93c6f8776234a8cd0633980
yangshuai <yangshuai@pachiratech.com>
2018-02-18 10:05:53 +0800

    Fix a bug when summary validation loss.

--- models/train.py
+++ models/train.py
@@ -17,13 +17,13 @@ def get_args():
     parser.add_argument("--save_path", type=str, default="./save/")
     parser.add_argument("--log_path", type=str, default="./log")
     parser.add_argument("--training_steps", type=int, default=250000)
-    parser.add_argument("--validation_steps", type=int, default=250)
+    parser.add_argument("--validation_size", type=int, default=152)
     parser.add_argument("--batch_size", type=int, default=1)
     parser.add_argument("--crop_length", type=int, default=8000)
     parser.add_argument("--sample_rate", type=int, default=20000)
     parser.add_argument("--add_audio_summary_per_steps", type=int, default=1000)
     parser.add_argument("--save_per_steps", type=int, default=5000)
-    parser.add_argument("--validation_per_steps", type=int, default=500)
+    parser.add_argument("--validation_per_steps", type=int, default=1000)
     return parser.parse_args()
 
 
@@ -39,7 +39,7 @@ def main():
             training_data = training_iterator.get_next()
 
         with tf.variable_scope("validation_data"):
-            validation_set = get_training_dataset(args.validation_data_path, args.batch_size, args.crop_length)
+            validation_set = get_training_dataset(args.validation_data_path, args.validation_size, args.crop_length)
             validation_set = validation_set.repeat()
             validation_iterator = validation_set.make_one_shot_iterator()
             validation_data = validation_iterator.get_next()
@@ -71,7 +71,8 @@ def main():
         if not load_model(saver, sess, save_path):
             sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
 
-        summary_writer = tf.summary.FileWriter(args.log_path)
+        training_writer = tf.summary.FileWriter(args.log_path + "/training", sess.graph)
+        validation_writer = tf.summary.FileWriter(args.log_path + "/validation")
 
         global_step_eval = sess.run(global_step)
         pbar = tqdm.tqdm(total=args.training_steps)
@@ -79,25 +80,27 @@ def main():
         while global_step_eval < args.training_steps:
             training_loss_summary_eval, audio_summary_eval, global_step_eval, _ = \
                 sess.run([training_loss_summary, audio_summary, global_step, upd])
-            summary_writer.add_summary(training_loss_summary_eval, global_step=global_step_eval)
+            training_writer.add_summary(training_loss_summary_eval, global_step=global_step_eval)
             """summary audio"""
-            if global_step_eval % args.add_audio_summary_per_steps == 0:
-                summary_writer.add_summary(audio_summary_eval, global_step=global_step_eval)
+            # if global_step_eval % args.add_audio_summary_per_steps == 0:
+            #     summary_writer.add_summary(audio_summary_eval, global_step=global_step_eval)
             """validate"""
             if global_step_eval % args.validation_per_steps == 0:
-                validation_loss_summary_eval = 0
-                for local_step_eval in range(args.validation_steps):
-                    validation_loss_summary_eval += sess.run([validation_loss_summary])
-                validation_loss_summary_eval /= args.validation_steps
-                # TODO calculate loss on whole validation set.
-                summary_writer.add_summary(validation_loss_summary_eval, global_step=global_step_eval)
+                # validation_loss_summary_eval = 0
+                # for local_step_eval in range(args.validation_steps):
+                # validation_loss_summary_eval += sess.run(validation_loss_summary)
+                # validation_loss_summary_eval /= args.validation_steps
+                validation_loss_summary_eval = sess.run(validation_loss_summary)
+                tf.logging.info("Validation done.")
+                validation_writer.add_summary(validation_loss_summary_eval, global_step=global_step_eval)
             """save model"""
             if global_step_eval % args.save_per_steps == 0:
                 if not os.path.exists(args.save_path) or not os.path.isdir(args.save_path):
                     os.makedirs(args.save_path)
                 save_model(saver, sess, save_path, global_step_eval)
             pbar.update(1)
-        summary_writer.close()
+        training_writer.close()
+        validation_writer.close()
 
     print("Congratulations!")
 
